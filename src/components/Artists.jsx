8d832bde041873d7613211f37304ec579fcab0a4
FMark207 <mark.farkas.207@gmail.com>
2025-04-28 10:20:12 +0200

    can switch between music

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,119 +1,38 @@
+import { useState } from "react"
 import Song from "./Song"
 
 function Artists() {
-    return(
-        <>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-                <Song name="Armed & Dangerous" artist="King Von" album="Welcome To O'Block" length="2:03" ></Song>
-        </>
+    const [activeIndex, setActiveIndex] = useState(null)
 
-        
+    const songs = [
+        { name: "Armed & Dangerous", artist: "King Von", album: "Welcome To O'Block", length: "2:03" },
+        { name: "Crazy Story", artist: "King Von", album: "Grandson, Vol. 1", length: "2:22" },
+        { name: "Took Her To The O", artist: "King Von", album: "Levon James", length: "3:12" },
+        { name: "How It Go", artist: "King Von", album: "Welcome To O'Block", length: "3:30" },
+        { name: "Wayne's Story", artist: "King Von", album: "Welcome To O'Block", length: "3:22" },
+        { name: "Why He Told", artist: "King Von", album: "Welcome To O'Block", length: "3:00" },
+        { name: "The Code", artist: "King Von", album: "Welcome To O'Block", length: "2:51" },
+        { name: "3 A.M.", artist: "King Von", album: "Levon James", length: "2:48" }
+    ]
+
+    function handleSongClick(index) {
+        setActiveIndex(index)
+    }
+
+    return (
+        <>
+            {songs.map((song, index) => (
+                <Song
+                    key={index}
+                    name={song.name}
+                    artist={song.artist}
+                    album={song.album}
+                    length={song.length}
+                    active={index === activeIndex}
+                    onClick={() => handleSongClick(index)}
+                />
+            ))}
+        </>
     )
 }
 
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,12 +3,20 @@ import styles from "./Song.module.css"
 
 function Song(props) {
     return (
-        <div className={styles.songContainer}>
+        <div className={styles.songContainer} onClick={props.onClick}>
             <img src={albumCover} />
-            <p className={styles.songName}>{props.name}</p>
-            <p className={styles.songArtist}>{props.artist}</p>
-            <p className={styles.songAlbum}>{props.album}</p>
-            <p className={styles.songLength}>{props.length}</p>
+            <p className={styles.songName} style={{ color: props.active ? "#1DD05D" : "#f3f3f3" }}>
+                {props.name}
+            </p>
+            <p className={styles.songArtist} style={{ color: props.active ? "#1CB351" : "#c2c2c2" }}>
+                {props.artist}
+            </p>
+            <p className={styles.songAlbum} style={{ color: props.active ? "#1DD05D" : "#f3f3f3" }}>
+                {props.album}
+            </p>
+            <p className={styles.songLength} style={{ color: props.active ? "#1CB351" : "#c2c2c2" }}>
+                {props.length}
+            </p>
         </div>
     )
 }
