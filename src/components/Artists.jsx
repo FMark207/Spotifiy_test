d0ca86d2958525e9fb043b703f7008ed4df1e4fb
FMark207 <mark.farkas.207@gmail.com>
2025-04-28 11:00:37 +0200

    can import songs

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,19 +1,11 @@
 import { useState } from "react"
 import Song from "./Song"
-
+import data from "../data.json" 
 function Artists() {
     const [activeIndex, setActiveIndex] = useState(null)
 
-    const songs = [
-        { name: "Armed & Dangerous", artist: "King Von", album: "Welcome To O'Block", length: "2:03" },
-        { name: "Crazy Story", artist: "King Von", album: "Grandson, Vol. 1", length: "2:22" },
-        { name: "Took Her To The O", artist: "King Von", album: "Levon James", length: "3:12" },
-        { name: "How It Go", artist: "King Von", album: "Welcome To O'Block", length: "3:30" },
-        { name: "Wayne's Story", artist: "King Von", album: "Welcome To O'Block", length: "3:22" },
-        { name: "Why He Told", artist: "King Von", album: "Welcome To O'Block", length: "3:00" },
-        { name: "The Code", artist: "King Von", album: "Welcome To O'Block", length: "2:51" },
-        { name: "3 A.M.", artist: "King Von", album: "Levon James", length: "2:48" }
-    ]
+    const kingVon = data.artists.find(artist => artist.name === "King Von")
+    const songs = kingVon ? kingVon.songs : []
 
     function handleSongClick(index) {
         setActiveIndex(index)
@@ -24,12 +16,14 @@ function Artists() {
             {songs.map((song, index) => (
                 <Song
                     key={index}
-                    name={song.name}
-                    artist={song.artist}
+                    name={song.title}
+                    artist="King Von"
                     album={song.album}
-                    length={song.length}
+                    length={song.duration}
                     active={index === activeIndex}
                     onClick={() => handleSongClick(index)}
+                    image={song.image}
+                    filename={song.filename}
                 />
             ))}
         </>
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,10 +1,14 @@
-import albumCover from "../covers/test.png"
 import styles from "./Song.module.css"
+import asd from "../covers/LevonFJames.png"
 
 function Song(props) {
+    
+    const str = props.album;
+    const modifiedStr = str.replace(/ /g, 'F');
+
     return (
         <div className={styles.songContainer} onClick={props.onClick}>
-            <img src={albumCover} />
+            <img src={`/src/covers/${modifiedStr}.png`}/>
             <p className={styles.songName} style={{ color: props.active ? "#1DD05D" : "#f3f3f3" }}>
                 {props.name}
             </p>
