25eaca3f81cf64a5ee5cb925ed6373438b4bf05c
FMark207 <mark.farkas.207@gmail.com>
2025-04-28 09:13:29 +0200

    Added modules

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,11 +1,14 @@
+import albumCover from "../covers/test.png"
+import styles from "./Song.module.css"
+
 function Song() {
-    return(
-        <div className="song-container">
-            <img src=""/>
-            <p className="song-name">Armed & Dangerous</p>
-            <p className="song-artist">King Von</p>
-            <p className="song-album">Welcome To O'Block</p>
-            <p className="song-length">2:03</p>
+    return (
+        <div className={styles.songContainer}>
+            <img src={albumCover} />
+            <p className={styles.songName}>Armed & Dangerous</p>
+            <p className={styles.songArtist}>King Von</p>
+            <p className={styles.songAlbum}>Welcome To O'Block</p>
+            <p className={styles.songLength}>2:03</p>
         </div>
     )
 }
